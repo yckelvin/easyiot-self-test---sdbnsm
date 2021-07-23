3ffb8ee25174f04f10e61d7c4267b3df7c88a470
Kelvin Lee <yckelvin@users.noreply.github.com>
2021-07-23 14:22:42 +0800

    Store password to variable and display on OLED

--- main.ts
+++ main.ts
@@ -3,13 +3,13 @@ microIoT.microIoT_MQTT_Event(microIoT.TOPIC.topic_0, function (message) {
     microIoT.microIoT_showUserText(0, "MQTT Received " + message)
     if (message == "smile") {
         basic.showIcon(IconNames.Happy)
-        microIoT.microIoT_showUserText(1, "Smile")
+        microIoT.microIoT_showUserText(2, "Smile")
     } else if (message == "sad") {
         basic.showIcon(IconNames.Sad)
-        microIoT.microIoT_showUserText(1, "Sad")
+        microIoT.microIoT_showUserText(2, "Sad")
     } else if (message == "door open") {
         basic.showIcon(IconNames.Square)
-        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
+        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 2)
     } else if (message == "door close") {
         basic.showIcon(IconNames.SmallSquare)
         microIoT.microIoT_ServoRun(microIoT.aServos.S1, 90)
@@ -17,14 +17,17 @@ microIoT.microIoT_MQTT_Event(microIoT.TOPIC.topic_0, function (message) {
 })
 input.onButtonPressed(Button.A, function () {
     microIoT.microIoT_showUserText(0, "Wifi: " + wifi_name)
+    microIoT.microIoT_showUserText(1, "Password: " + password)
 })
 input.onButtonPressed(Button.B, function () {
     microIoT.microIoT_clear()
 })
+let password = ""
 let wifi_name = ""
 microIoT.microIoT_initDisplay()
 wifi_name = "SDBNSM_DT Room"
-microIoT.microIoT_WIFI(wifi_name, "sdbnsmss")
+password = "sdbnsmss"
+microIoT.microIoT_WIFI(wifi_name, password)
 microIoT.microIoT_MQTT(
 "vkW338gnR",
 "DkZq38gnRz",
